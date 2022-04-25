729fa490229f88cb0629f64b8852ff61c3f2d8cd
Osvaldo Deneb <osvaldo.deneb@gmail.com>
2022-04-24 22:36:15 -0500

    añadir los productos al carrito y mostrarlos en la vista carrito

--- src/main/java/com/curso/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/curso/ecommerce/model/DetalleOrden.java
@@ -15,6 +15,7 @@ public class DetalleOrden {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String nombre;
 	private double cantidad;
 	private double precio;
 	private double total;
@@ -29,9 +30,10 @@ public class DetalleOrden {
 		
 	}
 
-	public DetalleOrden(Integer id, double cantidad, double precio, double total) {
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
 		super();
 		this.id = id;
+		this.nombre = nombre;
 		this.cantidad = cantidad;
 		this.precio = precio;
 		this.total = total;
@@ -43,6 +45,14 @@ public class DetalleOrden {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public double getCantidad() {
@@ -87,6 +97,6 @@ public class DetalleOrden {
 
 	@Override
 	public String toString() {
-		return "DetalleOrden [id=" + id + ", cantidad=" + cantidad + ", precio=" + precio + ", total=" + total + "]";
+		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio + ", total=" + total + "]";
 	}
 }
