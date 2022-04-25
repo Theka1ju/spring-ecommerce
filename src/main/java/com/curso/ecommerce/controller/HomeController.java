42ccaf466e6444e14f265b1072b1fabb7449a3fb
Osvaldo Deneb <osvaldo.deneb@gmail.com>
2022-04-24 21:48:36 -0500

    redireccion al carrito

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,5 +43,10 @@ public class HomeController {
 		model.addAttribute("producto", producto);
 		
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addcart() {
+		return "usuario/carrito";
 	}
 }
